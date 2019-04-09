f823543ce3bd8d2d0d8faead4aad548a860784a6
mohhmed Almawash <ksa.mo7md@gmail.com>
2019-04-09 20:56:36 +0300

    get data from data

--- feedr-starter-code/js/app.js
+++ feedr-starter-code/js/app.js
@@ -1,10 +1,4 @@
 var template = {
-  // createElement:(element,text)=>{
-  //   let el = document.createElement(element);
-  //   let t = document.createTextNode(text);
-  //   el.appendChild(t)
-  //   return el
-  // },
   createUl: (sources, parent) => {
     let createLi = (title, href) => {
       return `
@@ -15,18 +9,42 @@ var template = {
     };
     sources.forEach(source => {
       let liHtml = createLi(source.title, "#");
-      parent.insertAdjacentHTML('beforeend',liHtml);
+      parent.insertAdjacentHTML("beforeend", liHtml);
     });
   }
 };
-
+var api = {
+  getNews: source => {
+    try {
+      return fetch(source.link).then(res => {
+        return res.json().then(json => json.data);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
 (function() {
   var sources = [
-    { key: "reddit", title: "Reddit" },
-    { key: "mashable", title: "Mashable" },
-    { key: "digg", title: "Digg" }
+    {
+      key: "reddit",
+      title: "Reddit",
+      link: "http://digg.com/api/news/popular.json"
+    },
+    {
+      key: "mashable",
+      title: "Mashable",
+      link: "http://mashable.com/stories.json"
+    },
+    { key: "digg", title: "Digg", link: "http://mashable.com/stories.json" }
   ];
   // get DOMs
   var sourcesUl = document.getElementById("sourcesUl");
   template.createUl(sources, sourcesUl);
+  api.getNews(sources[0]).then(data => {
+    data = res;
+    console.log(data)
+  });
+  
+  
 })();
