a1d62e904f9c8792f3edd6b21dab24a808672998
mohhmed Almawash <ksa.mo7md@gmail.com>
2019-04-09 19:36:10 +0300

    create ul for sources

--- feedr-starter-code/js/app.js
+++ feedr-starter-code/js/app.js
@@ -1,3 +1,32 @@
-/*
-  Please add all Javascript code to this file.
-*/
+var template = {
+  // createElement:(element,text)=>{
+  //   let el = document.createElement(element);
+  //   let t = document.createTextNode(text);
+  //   el.appendChild(t)
+  //   return el
+  // },
+  createUl: (sources, parent) => {
+    let createLi = (title, href) => {
+      return `
+        <li>
+            <a href="${href}">${title}</a>
+        </li>
+      `;
+    };
+    sources.forEach(source => {
+      let liHtml = createLi(source.title, "#");
+      parent.insertAdjacentHTML('beforeend',liHtml);
+    });
+  }
+};
+
+(function() {
+  var sources = [
+    { key: "reddit", title: "Reddit" },
+    { key: "mashable", title: "Mashable" },
+    { key: "digg", title: "Digg" }
+  ];
+  // get DOMs
+  var sourcesUl = document.getElementById("sourcesUl");
+  template.createUl(sources, sourcesUl);
+})();
