3ae3186abe7be04fd9531648305b6e17ec929cf2
Abdulsalam Y Alahmri <salam9@outlook.com>
2019-04-12 03:19:31 +0300

    Article template

--- feedr-starter-code/js/app.js
+++ feedr-starter-code/js/app.js
@@ -4,6 +4,7 @@ var newsSource=[
   {Name: "NewsAPI",url: newsAPIurl},
   {Name: "Mashable",url: "http://mashable.com/stories.json"}
 ];
+articleinfo={image:'', url:'',title:'',tag:''};
 var current= newsSource[0];
 
 $(document).ready(function(){
@@ -41,20 +42,11 @@ function getNewsAPI(source){
   $("#main").html('');
   console.log(json.articles);
   $.each(json.articles, function(key, value) {
-    let Article = `<article class="article">
-    <section class="featuredImage">
-      <img src="${value.urlToImage}" alt="img" />
-    </section>
-    <section class="articleContent">
-        <a href="${value.url}"><h3>${value.title}</h3></a>
-        <h6>${value.source.name}</h6>
-    </section>
-    <section class="impressions">
-      526
-    </section>
-    <div class="clearfix"></div>
-  </article>`;
-  $("#main").append(Article);
+    articleinfo.image=value.urlToImage;
+    articleinfo.url=value.url;
+    articleinfo.title=value.title;
+    articleinfo.tag=value.source.name;
+  $("#main").append(getArticle(articleinfo));
   })
   });
 }
@@ -70,20 +62,11 @@ function getMashable(source){
   $("#main").html('');
   console.log(json.new);
   $.each(json.new, function(key, value) {
-    let Article = `<article class="article">
-    <section class="featuredImage">
-      <img src="${value.image}" alt="img" />
-    </section>
-    <section class="articleContent">
-        <a href="${value.link}"><h3>${value.title}</h3></a>
-        <h6>${value.channel}</h6>
-    </section>
-    <section class="impressions">
-      526
-    </section>
-    <div class="clearfix"></div>
-  </article>`;
-  $("#main").append(Article);
+    articleinfo.image=value.image;
+    articleinfo.url=value.link;
+    articleinfo.title=value.title;
+    articleinfo.tag=value.channel;
+  $("#main").append(getArticle(articleinfo));
   })
   });
 }
@@ -99,20 +82,26 @@ function getDIGG(source){
   $("#main").html('');
   console.log(json.data.feed);
   $.each(json.data.feed, function(key, value) {
-  let Article= `<article class="article">
-                    <section class="featuredImage">
-                      <img src="${value.content.media.images[0].url}" alt="img" />
-                    </section>
-                    <section class="articleContent">
-                        <a href="${value.content.url}"><h3>${value.content.title}</h3></a>
-                        <h6>${value.content.tags[0].display_name}</h6>
-                    </section>
-                    <section class="impressions">
-                      526
-                    </section>
-                    <div class="clearfix"></div>
-                  </article>`;
-  $("#main").append(Article);
+  articleinfo.image=value.content.media.images[0].url
+  articleinfo.url=value.content.url;
+  articleinfo.title=value.content.title;
+  articleinfo.tag=value.content.tags[0].display_name;
+  $("#main").append(getArticle(articleinfo));
   });
   });
+}
+function getArticle(article){
+  return `<article class="article">
+    <section class="featuredImage">
+      <img src="${article.image}" alt="img" />
+    </section>
+    <section class="articleContent">
+        <a href="${article.url}"><h3>${article.title}</h3></a>
+        <h6>${article.tag}</h6>
+    </section>
+    <section class="impressions">
+      526
+    </section>
+    <div class="clearfix"></div>
+  </article>`;
 }
