1ab5bb3a2c80e789f54836b03262a8e7abb4e75a
Fahad <falajmi2012@gmail.com>
2019-04-17 00:15:08 +0300

    second commit

--- feedr-starter-code/js/new.js
+++ feedr-starter-code/js/new.js
@@ -2,45 +2,31 @@
 $(function () {
     $("#popUp").removeClass("hidden");
     
-    var fahad = {
-      index : "",
-      title: "",
-     description: "",
-      imgSrc: "",
-      articleURL: ""
-    };
+    var fahad = [
+  ];
     function test(array_name, source) {
         var main_url = "https://newsapi.org/v1/articles?"
         var api_key = "apiKey=1f237dd5aac5431b86c2f5597690b2f6"
 
-        // var source = "source=CNN&ign&bbc-news&sortBy=random&";
         var url = main_url + source + api_key;
 
         $.ajax({
             type: 'GET',
             url: url,
-            contentType: "json",
+            // contentType: "json",
             success: function (data) {
                 $(".article").empty();
-                $(".loader").hide()
-
-
-                var data1 = data.articles
+                $(".loader").hide();
+                var data1 = data.articles ;     
                 data1.forEach((currentValue, index) => {
+                  
+                  console.log(currentValue);
                     let category = "lifestyle";
                     let title = currentValue.title;
                     let imgSrc = currentValue.urlToImage;
                     let articleURL = currentValue.url;
-                    let description = currentValue.description;
-                    fahad.title = title;
-                    fahad.description = description;
-                    fahad.imgSrc = imgSrc;
-                    fahad.index = index
-                    fahad.articleURL = articleURL;
-                    console.log(fahad);
-
-
-
+                    let description = currentValue.description;                  
+                    fahad.push(currentValue);
                     let newArticle = `
                     <article id=${index} class="article">
                         <section class="featuredImage">
@@ -58,14 +44,34 @@ $(function () {
                     `;
 
                     array_name.push(newArticle);
-                
                 })
                 $("#main").append(array_name);
+                $('.article').click(function() {
+                  event.preventDefault();
+                    console.log(this)
+                    var article_id = $(this).attr("id");
+                    var articleURL = $( `#${article_id} > .articleContent > a`).attr("href");
+                    // var articleimgSrc = $( `#${article_id} > .featuredImage > img`).attr("src");
+                    var articletitle = $( `#${article_id} > .articleContent > a > h3`).text();
+                    var articledescription = $( `#${article_id} > .articleContent > h6`).text();  
+                    $("#cont").empty();
+                  $(".loader").css("display", "block");
+                  $(".container").show();
+                  let art = `
+                        <article id=${article_id}>
+                        <h1>${articletitle}</h1>
+                        <p>
+                          ${articledescription}
+                        </p>
+                        <a href="${articleURL}" class="popUpAction" target="_blank">Read more from source</a>
+                    
+                      </div>                               
+                    `;
+                  $("#cont").append(art);
+                });
             },
             error: function (error) {
-                console.log("something went wrong please try again ")
-                console.log(error)
-            }
+                alert("something went wrong please try again ");            }
 
 
         })
@@ -80,7 +86,7 @@ $(function () {
 
     })
     $("#IGN").on("click", function() {
-        $("#popUp").removeClass("hidden");
+        // $("#popUp").removeClass("hidden");
         const arrayOfIGNArticles = [];
         test(arrayOfIGNArticles, "source=ign&sortBy=top&");
     
@@ -93,31 +99,15 @@ $(function () {
     $(".closePopUp").on("click", function() {
       $(".loader").hide();
     });
+    $("#search").on("click", function() {
+      $("#search").toggleClass("active");
 
-    $(".container").on("click", function(index) {
-        // const arrayOfpopUpArticles = [];
-        $("#cont").empty();
-      $(".loader").css("display", "block");
-      $(".container").show();
-
-      let art = `
-            <article id=${fahad.index}>
-            <h1>${fahad.title}</h1>
-            <p>
-              ${fahad.description}
-            </p>
-            <a href="${fahad.articleURL}" class="popUpAction" target="_blank">Read more from source</a>
-        
-          </div>
-
-       
-        
-        `;
-      $("#cont").append(art);
     });
+    $(".container a").on("click", function() {
+      var mainPage = window.location.href;
+      $(".container a").attr("href",mainPage);
+      window.location.reload()
+      console.log (mainPage);
 
-    
-    $("#s_image").on("click", function() {
-      console.log("hi fahad");
     });
 })
