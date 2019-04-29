bf54838f0fe682d2c455ac69ebdd6232328a0219
Ziyad S <ziyads.mansouri@gmail.com>
2019-04-29 19:29:10 +0300

    2nd commit

--- feedr-starter-code/js/app.js
+++ feedr-starter-code/js/app.js
@@ -28,7 +28,7 @@ masharray.push({
 title: results.new[i].display_title,
 url: results.new[i].link,
 description:results.new[i].content.plain,
-pic:results.new[i].image.url,
+pic:results.new[i].image,
 
 })
  console.log(masharray);
@@ -90,157 +90,157 @@ $('#popUp').addClass('loader hidden');
 }
 
 
-//// end of mash
+// end of mash
 
 
 
-// start of reddit
+//start of reddit
 
-// function redart(){
-//   $.get('https://www.reddit.com/top.json', function(results){
-// console.log(results);
+function redart(){
+  $.get('https://www.reddit.com/top.json', function(results){
+console.log(results);
 
-// for (var i=0 ; i<results.data.children.length; i++){
-// redarray.push({
-// title: results.data.children[i].data.title,
-// url: results.data.children[i].data.url,
-// description:results.data.children[i].data.title,
-// pic:results.data.children[i].data.url,
+for (var i=0 ; i<results.data.children.length; i++){
+redarray.push({
+title: results.data.children[i].data.title,
+url: results.data.children[i].data.url,
+description:results.data.children[i].data.title,
+pic:results.data.children[i].data.url,
 
-// })
-// // console.log(redarray);
-// } // for 
-// //append 
-// redarray.forEach( (article, index) => {   // arti al shay alli bnmshi 3leeh, index = i 
-//   let description = article.description
-//   let title = article.title
-//   let imgSrc = article.pic
+})
+// console.log(redarray);
+} // for 
+//append 
+redarray.forEach( (article, index) => {   // arti al shay alli bnmshi 3leeh, index = i 
+  let description = article.description
+  let title = article.title
+  let imgSrc = article.pic
 
-//   let newArticle = `
-//   <article id=${index} class="article"> 
-//     <section class="featuredImage">
-//       <img src="${imgSrc}" alt="" />
-//     </section>
-//     <section class="articleContent">
-//         <a href="#"><h3>${title}</h3></a>
-//         <h6>${description}</h6>
-//     </section>
-//     <section class="impressions">
-//       526
-//     </section>
-//     <div class="clearfix"></div>
-//     </article>
-//   `
-// $('#main').append(newArticle);
-//   // arrayOfjQueryArticles.push(newArticle)
-// })
+  let newArticle = `
+  <article id=${index} class="article"> 
+    <section class="featuredImage">
+      <img src="${imgSrc}" alt="" />
+    </section>
+    <section class="articleContent">
+        <a href="#"><h3>${title}</h3></a>
+        <h6>${description}</h6>
+    </section>
+    <section class="impressions">
+      526
+    </section>
+    <div class="clearfix"></div>
+    </article>
+  `
+$('#main').append(newArticle);
+  // arrayOfjQueryArticles.push(newArticle)
+})
 
-// //boooi   red on click 
+//boooi   red on click 
 
-// $('.article').on('click', function(event){
-// var r = $(this).attr('id');
-//   console.log(r);
-//   console.log(redarray[r]);
-//   $('#popUp').removeClass('loader hidden');
+$('.article').on('click', function(event){
+var r = $(this).attr('id');
+  console.log(r);
+  console.log(redarray[r]);
+  $('#popUp').removeClass('loader hidden');
 
-// let newpop=`
-// <a href="#" class="closePopUp">X</a>
-//           <div class="container">
-//             <h1>${redarray[r].title}</h1>
-//             <p>
-//               ${redarray[r].description}
-//             </p>
-//             <a href="${redarray[r].url}" class="popUpAction" target="_blank">Read more from source</a>
-//           </div>
-// `
-// $('#popUp').empty();
-// $('#popUp').append(newpop);
+let newpop=`
+<a href="#" class="closePopUp">X</a>
+          <div class="container">
+            <h1>${redarray[r].title}</h1>
+            <p>
+              ${redarray[r].description}
+            </p>
+            <a href="${redarray[r].url}" class="popUpAction" target="_blank">Read more from source</a>
+          </div>
+`
+$('#popUp').empty();
+$('#popUp').append(newpop);
 
-// $('.closePopUp').on('click', function(event){
-// $('#popUp').addClass('loader hidden');
+$('.closePopUp').on('click', function(event){
+$('#popUp').addClass('loader hidden');
 
-// //boooi
+//boooi
 
-// })
-// })
-// })
-// }
+})
+})
+})
+}
 
 //tgfelat al red 
 
-// DIG dig dig dig dig dig dig dig 
-// function digart(){
-//   $.get("https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json", function(results){
-// console.log(results);
+//DIG dig dig dig dig dig dig dig 
+function digart(){
+  $.get("https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json", function(results){
+console.log(results);
  
-// for (var i=0 ; i<results.data.feed.length; i++){
-// digarray.push({
-// title: results.data.feed[i].content.title,
-// url: results.data.feed[i].content.url,
-// description:results.data.feed[i].content.description,
-// pic:results.data.feed[i].content.media.images[0].url 
+for (var i=0 ; i<results.data.feed.length; i++){
+digarray.push({
+title: results.data.feed[i].content.title,
+url: results.data.feed[i].content.url,
+description:results.data.feed[i].content.description,
+pic:results.data.feed[i].content.media.images[0].url 
+
+})
+
+}
+
+// const arrayOfjQueryArticles = []
+
+digarray.forEach((article, index) => {   // arti al shay alli bnmshi 3leeh, index = i 
+  let description = article.description
+  let title = article.title
+  let imgSrc = article.pic
+
+  let newArticle = `
+  <article id=${index} class="article"> 
+    <section class="featuredImage">
+      <img src="${imgSrc}" alt="" />
+    </section>
+    <section class="articleContent">
+        <a href="#"><h3>${title}</h3></a>
+        <h6>${description}</h6>
+    </section>
+    <section class="impressions">
+      526
+    </section>
+    <div class="clearfix"></div>
+    </article>
+  `
+$('#main').append(newArticle);
+  // arrayOfjQueryArticles.push(newArticle)
+})
+
+ // brra al forEach 
+$('.article').on('click', function(event){
+var p = $(this).attr('id');
+  console.log(p);
+  console.log(digarray[p]);
+  $('#popUp').removeClass('loader hidden');
+
+let newpop=`
+<a href="#" class="closePopUp">X</a>
+          <div class="container">
+            <h1>${digarray[p].title}</h1>
+            <p>
+              ${digarray[p].description}
+            </p>
+            <a href="${digarray[p].url}" class="popUpAction" target="_blank">Read more from source</a>
+          </div>
+`
+$('#popUp').empty();
+$('#popUp').append(newpop);
+
+$('.closePopUp').on('click', function(event){
+$('#popUp').addClass('loader hidden');
 
 // })
 
-// }
+}) //on click closer (X)
 
-// // const arrayOfjQueryArticles = []
+  }) // on click article 
 
-// digarray.forEach((article, index) => {   // arti al shay alli bnmshi 3leeh, index = i 
-//   let description = article.description
-//   let title = article.title
-//   let imgSrc = article.pic
-
-//   let newArticle = `
-//   <article id=${index} class="article"> 
-//     <section class="featuredImage">
-//       <img src="${imgSrc}" alt="" />
-//     </section>
-//     <section class="articleContent">
-//         <a href="#"><h3>${title}</h3></a>
-//         <h6>${description}</h6>
-//     </section>
-//     <section class="impressions">
-//       526
-//     </section>
-//     <div class="clearfix"></div>
-//     </article>
-//   `
-// $('#main').append(newArticle);
-//   // arrayOfjQueryArticles.push(newArticle)
-// })
-
-//  // brra al forEach 
-// $('.article').on('click', function(event){
-// var p = $(this).attr('id');
-//   console.log(p);
-//   console.log(digarray[p]);
-//   $('#popUp').removeClass('loader hidden');
-
-// let newpop=`
-// <a href="#" class="closePopUp">X</a>
-//           <div class="container">
-//             <h1>${digarray[p].title}</h1>
-//             <p>
-//               ${digarray[p].description}
-//             </p>
-//             <a href="${digarray[p].url}" class="popUpAction" target="_blank">Read more from source</a>
-//           </div>
-// `
-// $('#popUp').empty();
-// $('#popUp').append(newpop);
-
-// $('.closePopUp').on('click', function(event){
-// $('#popUp').addClass('loader hidden');
-
-// // })
-
-// }) //on click closer (X)
-
-//   }) // on click article 
-
-//  }) // tgfelat al get )
-// } // tgfelat al function 
+ }) // tgfelat al get )
+} // tgfelat al function 
 
 $('#list1').on('click', function(event){
   // clean
